d37ee8c320650ba366563c2cd23a336b2b119e79
Angie <angie.valeriano@dekoding.pe>
2021-03-14 10:15:32 -0500

    Add circle shape on about us

--- poketlaw-frontend/src/views/AboutUs/index.jsx
+++ poketlaw-frontend/src/views/AboutUs/index.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import { useStyles } from './styles';
 import { Container } from '@material-ui/core';
+import clsx from 'clsx';
+import { Logo } from '../../Components/Logo';
 
 function AboutUs() {
   const classes = useStyles();
 
   return (
     <Container className={classes.baseContainer}>
+      <div className={classes.circleContainer}>
+        <div className={classes.logoContainer}>
+          <Logo />
+        </div>
+        <div className={clsx(classes.item, classes.deg0)} />
+        <div className={clsx(classes.item, classes.deg25)} />
+        <div className={clsx(classes.item, classes.deg45)} />
+        <div className={clsx(classes.item, classes.deg135)} />
+        <div className={clsx(classes.item, classes.deg180)} />
+        <div className={clsx(classes.item, classes.deg225)} />
+        <div className={clsx(classes.item, classes.deg90)} />
+        <div className={clsx(classes.item, classes.deg315)} />
+      </div>
     </Container>
   );
 }
--- poketlaw-frontend/src/views/AboutUs/styles.jsx
+++ poketlaw-frontend/src/views/AboutUs/styles.jsx
@@ -7,26 +7,47 @@ export const useStyles = makeStyles(theme => ({
     width: '100%',
     minHeight: '100%',
     justifyContent: 'flex-start',
-    alignItems: 'flex-end',
+    alignItems: 'center',
     flexDirection: 'column',
     padding: '3rem 4rem',
     [theme.breakpoints.down('sm')]: {
       justifyContent: 'flex-end',
     },
   },
-  lawsContainer:{
-    height: '10rem',
+  circleContainer: {
+    position: 'relative',
+    height: '35rem',
+    width: '35rem',
+    padding: '2.8em', /*= 2em * 1.4 (2em = half the width of an img, 1.4 = sqrt(2))*/
+    border: 'dashed 1px',
+    borderRadius: '50%',
+    margin: '1.75em auto 0',
   },
-  questionContainer: {
-    paddingTop: theme.spacing(5),
-    width: '100%',
+  item: {
+    backgroundColor: '#BAB4E2',
+    borderRadius: '50%',
+    display: 'block',
+    overflow: 'hidden',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: '8em',
+    height: '8em',
+    margin: '-4em', /* 2em = 4em/2 */ /* half the width */
   },
-  lawsQuestionContainer: {
-    height: '3rem',
-    margin: '1rem',
-    width: '100%',
-  },
-  accordion: {
-    width: '100%',
+  deg0: {transform: 'translate(18em)',}, /* 12em = half the width of the wrapper */
+  deg25: {transform: 'rotate(90deg) translate(18em) rotate(-90deg)',},
+  deg90: {transform: 'rotate(-90deg) translate(18em) rotate(90deg)',},
+  deg45: {transform: 'rotate(45deg) translate(18em) rotate(-45deg)',},
+  deg135: {transform: 'rotate(135deg) translate(18em) rotate(-135deg)',},
+  deg180: {transform: 'translate(-18em)',},
+  deg225: {transform: 'rotate(225deg) translate(18em) rotate(-225deg)',},
+  deg315: {transform: 'rotate(315deg) translate(18em) rotate(-315deg)',},
+  logoContainer: {
+    width: '21rem',
+    height: '7rem',
+    position: 'absolute',
+    top: '38%',
+    left: '21%',
   }
 }));
