e53ab9d4e3a6c101ef47b626b76ff2b0d1a18fbf
sergiorvs2 <sergio.valdivia@dekoding.pe>
2021-03-31 17:10:30 -0500

    added environ

--- pocketLaw_back_end/pocketLaw_back_end/settings.py
+++ pocketLaw_back_end/pocketLaw_back_end/settings.py
@@ -30,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w0x0#4jya@+m4pn0lf!6#u(bx+=y2o^2fj-*(l+hh!hig$$#f5'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
