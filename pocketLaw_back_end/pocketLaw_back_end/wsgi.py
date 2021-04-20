1d889e60c4c808e902e2ff1e988bb590833455ea
sergiorvs2 <sergio.valdivia@dekoding.pe>
2021-04-19 22:12:42 -0500

    modified wsgi

--- pocketLaw_back_end/pocketLaw_back_end/wsgi.py
+++ pocketLaw_back_end/pocketLaw_back_end/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pocketLaw_back_end.settings')
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE', 'pocketLaw_back_end.pocketLaw_back_end.settings')
 
 application = get_wsgi_application()
