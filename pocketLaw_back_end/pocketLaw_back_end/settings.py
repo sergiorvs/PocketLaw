061d9b8be314e6893ac9addff15bd75feafff713
sergiorvs2 <sergio.valdivia@dekoding.pe>
2021-04-19 19:16:35 -0500

    updated requiriments.txt

--- pocketLaw_back_end/pocketLaw_back_end/settings.py
+++ pocketLaw_back_end/pocketLaw_back_end/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pocketLaw_back_end.urls'
